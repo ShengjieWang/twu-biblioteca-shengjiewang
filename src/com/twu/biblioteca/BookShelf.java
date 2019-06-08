6f52d785281421e6efa064ed53f718f14de260dc
shengjiew2 <shengjiew2@student.unimelb.edu.au>
2019-06-08 12:48:28 +1000

    Borrow and return system done

--- src/com/twu/biblioteca/BookShelf.java
+++ src/com/twu/biblioteca/BookShelf.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 
 public class BookShelf {
     private ArrayList<Book> books = new ArrayList<Book>();
-
+    private ArrayList<Book> rentoutBooks = new ArrayList<Book>();
     public BookShelf(){
         for (int i=0; i<6; i++)
         {
             int initial_yy = 2000;
             Book currentBook  = new Book("Book"+(i+1),"Author"+(i+1), i +initial_yy,i );
-            this.books.add(currentBook);
+            this.books.add(i,currentBook);
         }
 
     }
@@ -20,11 +20,33 @@ public class BookShelf {
 
     public void printBooksName() {
         System.out.println("Currently available books are: ");
-        System.out.printf("%10s     %10s    %4s\n","Book Name","Author","Year");
+        System.out.printf("%10s     %10s     %10s    %4s\n","Book Id","Book Name","Author","Year");
         for (Book book : books)
         {
-            System.out.printf("%10s     %10s    %4d\n", book.name,book.author,book.publishedYear);
+            System.out.printf("%10d     %10s     %10s    %4d\n",book.id, book.name,book.author,book.publishedYear);
         }
     }
+    public boolean checkoutBook(int index) {
+        for (Book book : this.books) {
+            //Check the existence of the book
+            if (book.id == index) {
+                this.rentoutBooks.add(book);
+                this.books.remove(book);
+                return true;
+            }
+        }return false;
+    }
+    public boolean returnBook(int index) {
+        for (Book book : this.rentoutBooks) {
+            //Check the existence of the book
+            if (book.id == index) {
+                this.books.add(book);
+                this.rentoutBooks.remove(book);
+                return true;
+            }
+        }return false;
+    }
+
+
 
 }
